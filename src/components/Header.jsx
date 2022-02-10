23b9fe91d3318042a9d1a4ee8525ffd58b424914
Adriel Benjamín Martínez <61093891+Benjaminmq2013@users.noreply.github.com>
2022-02-11 00:00:26 +0100

    Title has been updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
         <img className="nasa_logo" src={ NASA_logo } alt="NASA logo" />
         <div>
             <h1 className="title">Aerial Images From Earth</h1>
-            <h2 className="subTitle">Enter Longitude and Latitude separated with slash</h2>
-            <h2 className="subTitle">Example: 120.32 / 47.44</h2>
+            <h2 className="subTitle">Enter Latitude and Longitude on each field</h2>
+            <h2 className="subTitle">Example: 13.69  -89.22</h2>
         </div>
     </div>
     
